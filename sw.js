b90e21412c83a0f5fadbd927537574bf8b43ee85
Alok Singh <3107aloksingh@gmail.com>
2026-08-01 20:29:25 +0530

    feat: Upgrade to Progressive Web App (PWA)

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "lectrack-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./calendar.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(ASSETS);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
